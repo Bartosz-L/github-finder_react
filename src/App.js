e10d2750c4c2aacff9a878c6a1b15db10d5a87a3
Bartosz Ligęza <bligeza@tidk.pl>
2019-08-06 12:36:48 +0200

    not found page and Home template

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import GithubState from 'context/github/GithubState';
 import AlertState from 'context/alert/AlertState';
 // components
 import Navbar from 'components/layout/Navbar';
-import Search from 'components/users/Search';
-import Users from 'components/users/Users';
 import User from 'components/users/User';
 import Alert from 'components/layout/Alert';
 // templates
+import Home from 'templates/Home';
 import About from 'templates/About';
+import NotFound from 'templates/NotFound';
 
 const App = () => {
   return (
@@ -23,18 +23,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <>
-                      <Search />
-                      <Users />
-                    </>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
